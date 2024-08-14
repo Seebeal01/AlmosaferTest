d13c076acad8fc9360da9453f9e363d387078cbf
Seebeal01 <seebbealabulila@gmail.com>
2024-08-14 22:01:41 +0300

    update

--- src/almosaferProject/FinallTest.java
+++ src/almosaferProject/FinallTest.java
@@ -25,16 +25,51 @@ public class FinallTest {
 
 	}
 
-	@Test
-	public void testAlmosaferAssertions() {
+	@Test(priority = 1)
+	public void testAlmosaferDefaultLanguageisEN() {
 
-	
-		WebElement language = driver.findElement(By.cssSelector(".sc-gkFcWv.jJNggu"));
-		String Actual = language.getText();
-		String expected = "العربية";
-		Assert.assertSame(Actual, expected);
-		
-		
+		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
+		Assert.assertEquals(ActualLanguage, ExpectedDefaultLanage);
+
+	}
+
+	@Test(priority = 2)
+	public void CheckdefaultCurrencyisSAR() {
+		String ExpectedCurrency = "SAR";
+		WebElement Currency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']"));
+
+		String ActualCurrency = Currency.getText();
+
+		Assert.assertEquals(ActualCurrency, ExpectedCurrency);
+	}
+
+	@Test(priority = 3)
+	public void CheckContactNumber() {
+		String ExpectedContactNumber = "+966554400000";
+		String ActualContactNumber = driver.findElement(By.tagName("strong")).getText();
+
+		Assert.assertEquals(ActualContactNumber, ExpectedContactNumber);
+	}
+
+	@Test(priority = 4)
+	public void CheckQitagLogo() {
+		boolean ExpectedResultsForTheLogo = true;
+		WebElement theFooter = driver.findElement(By.tagName("footer"));
+
+		WebElement logo = theFooter.findElement(By.cssSelector(".sc-fihHvN.eYrDjb"))
+				.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF"));
+
+		boolean ActualResultForThelogo = logo.isDisplayed();
+
+		Assert.assertEquals(ActualResultForThelogo, ExpectedResultsForTheLogo);
+
+	}
+	@Test(priority = 5)
+	public void checkHotel() {
+	WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+	String ActualSelectAreaValue = HotelTab.getAttribute("aria-selected");
+	String ExpectedSelectAreaValue = "false";
+	Assert.assertEquals(ActualSelectAreaValue,ExpectedSelectAreaValue);
 
 	}
 }
